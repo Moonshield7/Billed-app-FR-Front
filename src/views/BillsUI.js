76dba7c3c8fef96f8d1040cd532940514e2eb7e5
Moonshield7 <fannie.p@hotmail.fr>
2023-07-19 13:42:51 +0200

    Bills date sort issue

--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -19,13 +19,13 @@ const row = (bill) => {
     `)
   }
 
+
 const rows = (data) => {
-  return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
+  //Ajout d'un tri en fonction des dates, ordre décroissant (plus récent au plus ancien)
+  return (data && data.length) ? data.sort((a,b)=>{return new Date(b.date) - new Date(a.date)}).map(bill => row(bill)).join("") : ""
 }
 
-export default ({ data: bills, loading, error }) => {
-  console.log("honk", bills)
-  
+export default ({ data: bills, loading, error }) => {  
   const modal = () => (/*html*/`
     <div class="modal fade" id="modaleFile" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
